fd97e0edb52ed742520f5f32074d0f68b9ee7033
Fahim Khalili <KHA240518@spengergasse.at>
2025-10-05 10:14:09 +0200

    neue Code Zeilen wurden hinzugefügt.

--- Student.java
+++ Student.java
@@ -9,6 +9,7 @@ public class Student{
         setAlter(alter);
         setMatura(matura);
     }
+    
     public Student(String name, int alter){
         setName(name);
         setAlter(alter);
@@ -56,6 +57,50 @@ public class Student{
         this.matura = matura;
     }
     
+    public String getInitialen(){
+        String initialen;
+        int pos; /* Diese Zahl merkt sich die Position des Leerzeichens im Namen.*/
+        
+        initialen = name.substring(0,1); 
+        pos = name.indexOf(" ")+1;
+        /* Der Code sucht nach dem Leerzeichen 
+        im Namen (zwischen Vor- und Nachname) und geht einen Schritt weiter – 
+        also zur ersten Stelle des Nachnamens.*/
+        
+        initialen = initialen + name.substring(pos,pos + 1);
+        /* weil pos ist der Stelle der erstes Buchstabe und pos + 1 ist der
+           nächster Wert*/
+        return initialen;
+        
+    }
+    
+    public String getVorname(){
+        int pos;
+        
+        pos = name.indexOf(" ");
+        return name.substring(0,pos);
+        
+    }
+    
+    public String getNachname(){
+        int pos;
+        
+        pos = name.indexOf(" ") + 1;
+        return name.substring(pos);
+    }
+    
+    public String getNachnameVorname(){
+        return getNachname() + " " + getVorname();
+    }
+    
+    public String getAccount(){
+        String nachname, nachname4;
+        nachname = getNachname();
+        nachname4 = nachname.substring(0,4);
+        return nachname4.toUpperCase() + alter;
+        
+        /* zum Lernen, nicht ganz klar*/
+    }
     public void printStudent(){
         if(matura == true){
             System.out.println("Name:" + name + "." + "Alter:" + alter + 
